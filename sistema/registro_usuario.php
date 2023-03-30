de8c6da524cd32d871d4a980f7b3a1113a34698f
Franc <angelvargasherasme@gmail.com>
2023-03-29 20:12:27 -0400

    cambios por registro de usuario y arhivo txt

--- sistema/registro_usuario.php
+++ sistema/registro_usuario.php
@@ -2,10 +2,32 @@
     if(!empty($_POST))
     {
         $alert='';
-        if(empty($_POST['nombre'] || empty($_POST['correo'] || empty($_POST['usuario'] || empty($_POST['clave'] || empty($_POST['rol']))
-
+        if(empty($_POST['nombre']) || empty($_POST['correo']) || empty($_POST['usuario']) || empty($_POST['clave']) || empty($_POST['rol']))
         {
-            $alert='<p class="msg_error"></p>';
+            $alert='<p class="msg_error">Todos los campos son obligatorios</p>';
+        }else{
+            include "..\conexion.php";
+            $nombre = $_POST['nombre'];
+            $email = $_POST['correo'];
+            $user = $_POST['usuario'];
+            $clave = md5($_POST['clave']);
+            $rol = $_POST['rol'];
+
+            $query = mysqli_query($conexion, "SELECT * FROM usuario WHERE usuario = '$user' OR correo = '$email'");
+            $result = mysqli_fetch_array($query);
+
+            if($result > 0){
+                $alert='<p class="msg_error">El correo o el usuario ya existe.</p>';
+            }else{
+                $query_insert= mysqli_query($conexion, "INSERT INTO usuario(nombre,correo,usuario,clave,rol) 
+                                                                    VALUES('$nombre', '$email', '$user', '$clave', '$rol')");
+                if($query_insert){
+                    $alert='<p class="msg_save">Usuario creado correctamente.</p>';
+                }else{
+                    $alert='<p class="msg_error">Error al crear el usuario.</p>';
+                }
+            }
+            
         }
    }
 ?>
@@ -20,7 +42,6 @@
     <?php include "includes/scripts.php"; ?>
     <title>Sisteme Ventas</title>
 </head>
-<p>hola</p>
 <body>
     <?php include "includes/header.php";?>
     <section id="container">
@@ -29,21 +50,21 @@
                 <div class="col-md-12 bg-white p-5">
                     <center>
                     <h3 class="pb-3">Registro</h3>
-                    <div class="alerta"><p class="msg_save">Error</p> </div> 
+                    <div class="alerta"><?php echo isset($alert) ? $alert :'' ?></div> 
                     </center>
                 <div>
                     <form action="" method="post">
                         <div class="form-group pb-3">
-                            <input type="text" placeholder="Nombre completo" class="form-control" name="nombre" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            <input type="text" placeholder="Nombre completo" class="form-control" name="nombre" id="nombre" aria-describedby="emailHelp">
                         </div>
                         <div class="form-group pb-3">
-                            <input type="text" placeholder="Correo" class="form-control" name="correo" id="exampleInputPassword1">
+                            <input type="text" placeholder="Correo" class="form-control" name="correo" id="correo">
                         </div>
                         <div class="form-group pb-3">
-                            <input type="text" placeholder="Usuario" class="form-control" name="usuario" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            <input type="text" placeholder="Usuario" class="form-control" name="usuario" id="usuario" aria-describedby="emailHelp">
                         </div>
                         <div class="form-group pb-3">
-                            <input type="password" placeholder="Contraseña" class="form-control" name="clave" id="exampleInputPassword1">
+                            <input type="password" placeholder="Contraseña" class="form-control" name="clave" id="clave">
                         </div>
                             <div class="form-group pb-3">
                                 <label for="rol">Tipo de Usuario</label>
